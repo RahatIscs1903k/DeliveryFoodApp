40e12823bc902ef22f0afa4d12f2e2978f933e27
yessenali_54782 <Rakhat.Yessenali@Kaspi.kz>
2024-05-25 20:07:01 +0500

    Created base project, remove storyboard references

---
+++ FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodDeliveryApp
+//
+//  Created by Kaspi on 25.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        
+    }
+
+}
+
